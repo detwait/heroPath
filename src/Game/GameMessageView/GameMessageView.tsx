a83aa6912b04de63a6b6352a3e4f540c9019fd58
Пользователь <you@example.com>
2022-10-18 04:40:52 +0400

    game preview screen

--- src/Game/GameMessageView/GameMessageView.tsx
+++ src/Game/GameMessageView/GameMessageView.tsx
@@ -2,6 +2,7 @@ import './GameMessageView.css';
 import { GameMessageViewProps } from './GameMessageView.props';
 import { Config } from '../../Config';
 import { ImageService, ImageType } from '../../_Shared/image';
+import { GameStatus } from '../GameStatus.enum';
 
 const imageService: ImageService = new ImageService();
 
@@ -11,14 +12,21 @@ export function GameMessageView({ gameStatus, startGame }: GameMessageViewProps)
       <h2>
         { Config.gameMessage[gameStatus] }
       </h2>
-      <p>
-        <img 
-          className='StartGame'
-          src={`${imageService.getPath(ImageType.action, 'start.png')}`} 
-          alt="start" 
-          onClick={() => startGame()}
-        />
-      </p>
+      {(() => {
+        if (gameStatus === GameStatus.preview) {
+          return (
+            <p>
+            <img 
+              className='StartGame'
+              src={`${imageService.getPath(ImageType.action, 'start.png')}`} 
+              alt="start" 
+              onClick={() => startGame()}
+            />
+          </p>
+          )
+        }
+      })()}
+
     </div>
   );
 };
